e6b4d92302006dc31bd4ebd5edfed4f05cfe214a
Kirill Kouzoubov <kirill888@gmail.com>
2019-10-12 23:51:22 +1100

    Switch to iterator based design
    Adding with_ui_events that wraps an iterator, change ui_poll to use
    with_ui_events under the hood.

--- jupyter_ui_poll/__init__.py
+++ jupyter_ui_poll/__init__.py
@@ -1,8 +1,9 @@
 """ Block notebook cells from running while interacting with widgets
 """
 
-from ._poll import ui_poll
+from ._poll import ui_poll, with_ui_events
 
 __all__ = (
     "ui_poll",
+    "with_ui_events",
 )
--- jupyter_ui_poll/_poll.py
+++ jupyter_ui_poll/_poll.py
@@ -17,47 +17,72 @@ def _replay_events(shell, events):
             break
 
 
-def _ui_poll(f, sleep):
+def with_ui_events(its, n=1):
+    """ Deal with kernel ui events while processing a long sequence
+
+    :param its: Iterator to pass through
+    :param n:   Number of events to process in between items
+
+    - Delay processing `execute_request` IPython kernel events
+    - Inject calls to `kernel.do_one_iteration()` in between iterations
+    - Schedule replay of any blocked `execute_request` events when data sequence is exhausted
+    """
     shell = get_ipython()
     kernel = shell.kernel
     events = []
     kernel.shell_handlers['execute_request'] = lambda *e: events.append(e)
     current_parent = (kernel._parent_ident, kernel._parent_header)
-    shell.execution_count += 1
+    # shell.execution_count += 1
 
-    def finalise():
-        _replay_events(shell, events)
+    def cleanup():
+        kernel.shell_handlers['execute_request'] = kernel.execute_request
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            loop.call_soon(lambda: _replay_events(shell, events))
+        else:
+            warn(
+                'Automatic execution of scheduled cells only works with asyncio based ipython'
+            )
 
     try:
-        x = f()
-        while x is None:
-            kernel.do_one_iteration()
-            kernel.set_parent(*current_parent
-                              )  # ensure stdout still happens in the same cell
-            if sleep is not None:
-                time.sleep(sleep)
-            x = f()
-        return x, finalise
+        for x in its:
+            for _ in range(n):
+                kernel.do_one_iteration()
+                # ensure stdout still happens in the same cell
+                kernel.set_parent(*current_parent)
+
+            yield x
+    except GeneratorExit:
+        pass
     except Exception as e:
         raise e
     finally:
-        kernel.shell_handlers['execute_request'] = kernel.execute_request
+        cleanup()
 
 
-def ui_poll(f, sleep=0.02):
+def ui_poll(f, sleep=0.02, n=1):
     """Repeatedly call `f()` until it returns non-None value while also responding to widget events.
 
     This blocks execution of cells below in the notebook while still preserving
     interactivity of jupyter widgets.
+
+    :param f: Function to periodically call (`f()` should not block for long)
+    :param sleep: Amount of time to sleep in between polling (in seconds, 1/50 is the default)
+    :param n: Number of events to process per iteration
+
+    Returns
+    =======
+    First non-None value returned from `f()`
     """
+    def as_iterator(f, sleep):
+        x = None
+        while x is None:
+            if sleep is not None:
+                time.sleep(sleep)
 
-    x, finalise = _ui_poll(f, sleep)
-    loop = asyncio.get_event_loop()
-    if loop.is_running():
-        loop.call_soon(finalise)
-    else:
-        warn(
-            'Automatic execution of scheduled cells only works with asyncio based ipython'
-        )
+            x = f()
+            yield x
 
-    return x
+    for x in with_ui_events(as_iterator(f, sleep), n):
+        if x is not None:
+            return x
