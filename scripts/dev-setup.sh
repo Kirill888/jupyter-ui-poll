9e53656714f0e1b57803f219791983793625afbe
Kirill Kouzoubov <kirill888@gmail.com>
2019-10-26 15:35:53 +1100

    Make jupyterlab install optional in dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -5,7 +5,11 @@ python3 -m venv "${env_dir}"
 source "${env_dir}/bin/activate"
 pip install -U pip
 pip install -e .
-pip install jupyter jupyterlab ipyleaflet
-jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager
-jupyter labextension install --no-build jupyter-leaflet
-jupyter lab build
+pip install jupyter ipywidgets ipyleaflet
+
+if [ "${2:-notebook}" = "lab" ]; then
+  pip install jupyterlab
+  jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager
+  jupyter labextension install --no-build jupyter-leaflet
+  jupyter lab build
+fi
