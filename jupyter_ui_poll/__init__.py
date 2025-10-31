f6fcef8c15bb516dac72e13be04af2b7080a7b56
Kirill Kouzoubov <kirill888@gmail.com>
2025-10-31 16:42:46 +1100

    maint: switch to flit for packaging

--- jupyter_ui_poll/__init__.py
+++ jupyter_ui_poll/__init__.py
@@ -1,12 +1,22 @@
-""" Block notebook cells from running while interacting with widgets
-"""
+"""Block notebook cells from running while interacting with widgets"""
 
-from ._version import __version__
 from ._poll import ui_events, with_ui_events, run_ui_poll_loop
 
 __all__ = (
     "ui_events",
     "with_ui_events",
     "run_ui_poll_loop",
-    "__version__",
 )
+
+
+def __dir__():
+    return [*__all__, "__version__"]
+
+
+def __getattr__(name):
+    # pylint: disable=import-outside-toplevel
+    if name == "__version__":
+        from importlib.metadata import version
+
+        return version(__name__)
+    raise AttributeError(f"module {__name__} has no attribute {name}")
